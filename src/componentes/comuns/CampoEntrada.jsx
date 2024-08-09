8da436e9ab278ca7c6ce5dd7c8546b6c8a21df30
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-08-09 17:04:24 -0300

    refactor: refactor fields components

--- src/componentes/comuns/CampoEntrada.jsx
+++ src/componentes/comuns/CampoEntrada.jsx
@@ -4,25 +4,23 @@ import Box from '@mui/material/Box';
 function CampoEntrada({ id, label, tipo, name, value, onchange, requerido
     , readonly, maxlength, msgvalido, msginvalido, placeholder }) {
     return (
-        <>
-                <TextField
-                    InputProps={{
-                        readOnly: readonly
-                    }}
-                    fullWidth={true}
-                    type={tipo}
-                    id={id}
-                    label={label}
-                    placeholder={placeholder}
-                    name={name}
-                    value={value}
-                    onChange={onchange}
-                    required={requerido}
-                    maxLength={maxlength}
-                    helperText={value === "" && requerido===true ? msginvalido : ' '}
-                    error={readonly || requerido===false ? false : value === ""}
-                />
-        </>
+        <TextField
+            InputProps={{
+                readOnly: readonly
+            }}
+            fullWidth={true}
+            type={tipo}
+            id={id}
+            label={label}
+            placeholder={placeholder}
+            name={name}
+            value={value}
+            onChange={onchange}
+            required={requerido}
+            maxLength={maxlength}
+            helperText={value === "" && requerido === true ? msginvalido : ' '}
+            error={readonly || requerido === false ? false : value === ""}
+        />
     )
 }
 
